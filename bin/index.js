37a18ece22397b28fc6ecce293d8314a12ff0f8e
German RIO <german@germanrio.es>
2015-08-11 23:47:27 +0100

    Check waypoints

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,13 @@
 #! /usr/bin/env node
 
-console.log('gpx');
+var yargs = require('yargs'),
+    gpsChecker = require('../');
+
+var argv = yargs
+    .usage('Check if your gpx track has been recorded properly.\nUsage: gpx-checker <file>')
+    .demand(1, 1, 'You should provide a gpx track file.')
+    .help('help')
+    .alias('h', 'help')
+    .argv;
+
+gpsChecker(argv._[0]);
