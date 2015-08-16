6f2b0aabfd537349045ab87fb2da2f4e69bbef98
German RIO <german@germanrio.es>
2015-08-16 11:25:41 +0100

    Use promises

--- bin/index.js
+++ bin/index.js
@@ -6,8 +6,16 @@ var yargs = require('yargs'),
 var argv = yargs
     .usage('Check if your gpx track has been recorded properly.\nUsage: gpx-checker <file>')
     .demand(1, 1, 'You should provide a gpx track file.')
+    .option('output', {
+      alias: 'o',
+      describe: 'Output file path to save corrected track',
+      type: 'string'
+    })
     .help('help')
     .alias('h', 'help')
     .argv;
 
-gpsChecker(argv._[0]);
+gpsChecker({
+  file: argv._[0],
+  output: argv.output
+});
